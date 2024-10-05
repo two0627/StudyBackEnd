55b1c764d0762f494e4adc0dd8890079c21d0793
two0627 <two0627@naver.com>
2024-10-05 20:36:19 +0900

    유효성 검증, db 쿼리 분리 및 서버 시작 변경

--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -1,15 +1,29 @@
-// controllers/userController.js
 const User = require('./UserModel.js');
 
+// 유효성 검증 함수
+const validateLoginInput = (username, password) => {
+  if (!username || !password) {
+    return { valid: false, message: '아이디와 비밀번호를 입력해주세요.' };
+  }
+  return { valid: true };
+};
+
+// 로그인 함수
 const login = (req, res) => {
   const { username, password } = req.body;
 
-  // 데이터베이스에서 유저 찾기
+  // 1. 유효성 검증 파트
+  const validation = validateLoginInput(username, password);
+  if (!validation.valid) {
+    return res.status(400).json({ message: validation.message });
+  }
+
+  // 2. 데이터베이스 쿼리 파트
   User.findByUsername(username, (err, user) => {
     if (err) {
       return res.status(500).json({ error: '서버 에러' });
     }
-    
+
     // 아이디가 존재하지 않을 경우
     if (!user) {
       return res.status(400).json({ message: '아이디가 존재하지 않습니다.' });
